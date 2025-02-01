301c92de9351bcd310a6cf32db137aefa895aa1d
0101error <tamoy.c1@yahoo.com>
2025-01-31 21:59:48 -0500

    Implement temperature check function

--- app.py
+++ app.py
@@ -13,3 +13,23 @@ def potential_divider(voltage_supply, series_resistances):
         print(f'"{voltage_drop:.2f}v"')
 
 potential_divider(9, [3000, 1000])
+
+def temperature_check(temp, unit):
+    if unit == "C":
+        if temp < 36:
+            print("The patient is hypothermic")
+        elif temp > 37.5:
+            print("The patient is hyperthermic")
+        else:
+            print("The patient's temperature is normal")
+    else:  
+        if temp < 96.8:
+            print("The patient is hypothermic")
+        elif temp > 99.5:
+            print("The patient is hyperthermic")
+        else:
+            print("The patient's temperature is normal")
+
+temperature_check(14, "C")  
+temperature_check(37, "C") 
+temperature_check(37, "F")  
