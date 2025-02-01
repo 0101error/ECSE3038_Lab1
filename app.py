d1eb4ce7351b65891c55285af81c87d9109b181d
0101error <tamoy.c1@yahoo.com>
2025-01-31 21:48:45 -0500

    Implement potential divider function

--- app.py
+++ app.py
@@ -4,3 +4,12 @@ def parallel(resistances):
     print(f'"{results:.3f} ohms"')
 
 parallel([330, 1000, 2200])
+
+def potential_divider(voltage_supply, series_resistances):
+
+    total_resistance = sum(series_resistances)
+    for r in series_resistances:
+        voltage_drop = (r / total_resistance) * voltage_supply
+        print(f'"{voltage_drop:.2f}v"')
+
+potential_divider(9, [3000, 1000])
